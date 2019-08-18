0c9ad049bc8abf8b2b14036d6002866cd7ef16ab
William McFall <wmcfall2@gmail.com>
2019-08-18 17:09:22 -0400

    added validation to user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,11 @@ const {check, validationResult} = require("express-validator/check");
 // @route           route post api/users
 // @description     register user
 // @access          public access
-router.post("/", (req,res) => {
+router.post("/", [
+    check("name", "Please include valid name").not().isEmpty(),
+    check("email","Please include valid email address").isEmail(),
+    check("password","Please include password of at least 6 characters").isLength({min:6}),
+], (req,res) => {
     console.log(req.body)
     res.send("User route")
 });
