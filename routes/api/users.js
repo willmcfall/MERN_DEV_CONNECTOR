54b0b4503523b45f11c59863eeb57ee5172987bf
William McFall <wmcfall2@gmail.com>
2019-08-18 17:18:20 -0400

    added validation to user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {check, validationResult} = require("express-validator/check");
+const {check, validationResult} = require("express-validator");
 
 // @route           route post api/users
 // @description     register user
@@ -10,6 +10,10 @@ router.post("/", [
     check("email","Please include valid email address").isEmail(),
     check("password","Please include password of at least 6 characters").isLength({min:6}),
 ], (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
     console.log(req.body)
     res.send("User route")
 });
