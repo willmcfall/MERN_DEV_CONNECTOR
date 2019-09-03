4c020661d7444101df497cbb17b239fd41493af8
William McFall <wmcfall2@gmail.com>
2019-09-02 22:12:53 -0400

    added profile add and update routing

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -35,7 +35,66 @@ router.post("/", [auth,[check("status", "Status is required").not().isEmpty(),ch
   if(!errors.isEmpty()){
     return res.status(400).json({errors: errors.array()})
   }
-});
+
+  const {
+    company,
+    website,
+    location,
+    bio,
+    status,
+    githubusername,
+    skills,
+    youtube,
+    facebook,
+    twitter,
+    instagram,
+    linkedin
+  } = req.body
+
+  // build profile object
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if(company) profileFields.company = company;
+  if(website) profileFields.website = website;
+  if(location) profileFields.location = location;
+  if(bio) profileFields.bio = bio;
+  if(status) profileFields.status = status;
+  if(githubusername) profileFields.githubusername = githubusername;
+  if(skills){
+    profileFields.skills = skills.split(',').map(skills => skills.trim())
+  }
+
+  // build social object
+  profileFields.social ={}
+    if(youtube) profileFields.social.youtube = youtube;
+    if(facebook) profileFields.social.facebook = facebook;
+    if(twitter) profileFields.social.twitter = twitter;
+    if(instagram) profileFields.social.instagram = instagram;
+    if(linkedin) profileFields.social.linkedin = linkedin;
+  ;
+
+ try{
+  let profile = await Profile.findOne({user:req.user.id})
+  if(profile){
+    // update
+    profile = await Profile.findOneAndUpdate({user:req.user.id},{$set: profileFields},{new:true})
+
+    return res.json(profile);
+
+  }
+
+  profile = new Profile(profileFields);
+
+  await profile.save();
+
+  return res.json(profile);
+
+ } catch(err){
+  console.error(err.message);
+  res.status(500).send('Server error')
+ }
+}
+);
 
 
 
